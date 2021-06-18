fb0c09e062bfc0de667106b07a7b2ffcbf312942
piForever <pilarbenito98@hotmail.com>
2021-06-18 18:45:24 -0300

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from "react"; 
+import SearchBar from '../SearchBar/SearchBar'; 
+import BusinessList from '../BusinessList/BusinessList';
+import './App.css';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+    <h1>Netflix Search</h1>
+    <SearchBar />
+    <BusinessList />
+  </div>
+    );
+  } 
+}
+
+export default App;
